b61a719c8e92af9214c80464f5b20e0ed9bd10f4
srezasm <srezas.2017@gmail.com>
2023-08-04 18:40:08 +0330

    saved mu, included datasets, fixed movieId

--- jupyter-docs/settings.py
+++ jupyter-docs/settings.py
@@ -1,6 +1,11 @@
+MOVIES_CSV_FILE = 'ml-latest-small/movies.csv'
+RATINGS_CSV_FILE = 'ml-latest-small/ratings.csv'
+TAGS_CSV_FILE = 'ml-latest-small/tags.csv'
+
 Y_FILE_NAME = 'Y.npz'
 R_FILE_NAME = 'R.npz'
 X_M_FILE_NAME = 'X_m.npz'
+MU_FILE_NAME = 'mu.npz'
 
 W_FILE_NAME = 'W.pt'
 X_FILE_NAME = 'X.pt'
